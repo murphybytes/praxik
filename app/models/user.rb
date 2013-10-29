273f6309487037a7e1e346491ea475f6a7c47b11
faust45 <faust451@gmail.com>
2013-10-29 20:16:49 +0200

    add auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :rememberable, :trackable
 
   store :data, accessors: [:first_name, :last_name, :primary_phone, :secondary_phone]
+
+  def to_s
+    login
+  end
 
 end
