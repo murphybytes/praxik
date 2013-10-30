c91f6ad700589b4683852256d120596846588f08
Justin Stevens <justinstevens@itracedesigns.com>
2013-10-30 14:13:54 -0500

    Pulled out inline JS

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -2,3 +2,39 @@
 //= require jquery_ujs
 
 
+
+    //Adds user friendly button submission animation
+    function reset_submit_button(){
+        jQuery(document).find("input[type=submit]").removeAttr("disabled"); 
+        jQuery("#form-processing-animation").hide();
+    }
+    jQuery(document).ready(function() {     
+        jQuery("#my_fields").bind("submit", function() { 
+            jQuery("#form-processing-animation").show();
+            jQuery(this).find("input[type=submit]").attr("disabled", "disabled"); 
+        });
+    });
+
+    jQuery(document).ready( function() {
+        jQuery("#view-field-table").tablesorter({  
+            sortList: [[1,0]],
+            widgets: ['zebra'],
+            headers:{
+                0:{
+                    sorter: false
+                },
+                4:{
+                    sorter: false
+                }
+            }
+        });
+        jQuery("#my_fields")
+            .submit( function() {   
+                if(jQuery("#selected_action").val() == "delete")    {
+                    return confirm("This cannot be undone. Do you wish to continue?")
+                }else{
+                    jQuery('input[type="submit"]').removeAttr('disabled');
+                    return false;
+                }
+        });
+    });
