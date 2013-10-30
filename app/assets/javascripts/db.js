894f72df5ee7b297577f44b5fa2087a477b4cafc
faust45 <faust451@gmail.com>
2013-10-30 20:49:24 +0200

    bunch of fixes

--- app/assets/javascripts/db.js
+++ app/assets/javascripts/db.js
@@ -7,7 +7,10 @@ function DB($resource) {
     var options = {
     };
 
-    var Doc = $resource("/account/fields/:id");
+    var Doc = $resource("/account/fields/:id",
+            {id: '@id'},
+            {update: {method:'PUT', params:{}, isArray:false}});
+             
 
     return Doc;
 }
