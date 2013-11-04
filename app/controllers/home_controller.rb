36a045ced9e714651f9e3c89282a33fd1ee9e265
Justin Stevens <justinstevens@itracedesigns.com>
2013-11-04 12:27:19 -0600

    Set up linking to static pages if user is not signed in

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   layout "main"
 
+  def home
+    render "public/pages/home.html"
+  end
+
   def services
     render "public/pages/services.html"
   end
